5152a516649f95199cb7cf826f52d94eacd782f8
Robiul Hassan <robiul.hassan12102@gmail.com>
2025-12-24 01:27:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7914018631-f54bd",
+  "appId": "1:359337308508:web:c2c226a1a09dc2008f9199",
+  "apiKey": "AIzaSyAmcLdjwCDYXUmPmjX1zEfP-9FdFMj_dpw",
+  "authDomain": "studio-7914018631-f54bd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "359337308508"
+};
